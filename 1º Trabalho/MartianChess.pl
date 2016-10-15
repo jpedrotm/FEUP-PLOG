e854266ab470b2184e9ea6dfbca0772a150c01b5
jpedrotm <j.pedroteixeira.monteiro@gmail.com>
2016-10-15 23:32:33 +0100

    Display improve and calc dinamic board

--- 1º Trabalho/MartianChess.pl
+++ 1º Trabalho/MartianChess.pl
@@ -1,37 +1,114 @@
-/*Q-Queen-3
-  D-Drones-2
-  P-Peão-1
-*/
+
 
 main:-
-  initialize_board(Board),
-  display_board(Board,8).
+  Rows = 8, Columns = 4,
+  generate_board(Matrix,Rows,Columns),
+  display_cols(0,Columns),
+  display_board(Matrix,Rows,Columns,0).
 
-initialize_board(Board):-
-  Board=[['Q','Q','D',' '],
-         ['Q','D','P',' '],
-         ['D','P','P',' '],
-         [' ',' ',' ',' '],
-         [' ',' ',' ',' '],
-         [' ','P','P','D'],
-         [' ','P','D','Q'],
-         [' ','D','Q','Q']].
+  /*Função para inicializar um tabuleiro de forma estática-----------------------------------------------------------------------------------------------------------------------------*/
 
-display_board([L|Ls],N):-N\=5,N1 is N-1,display_line(L),nl,display_board(Ls,N1).
-display_board([L|Ls],N):-N1 is N-1,display_line(L),write(' '),display_horizontal(6),nl,nl,display_board(Ls,N1).
-display_board([],0):-nl.
+  initialize_board(Board):-
+    Board=[['Q','Q','D',' '],
+           ['Q','D','P',' '],
+           ['D','P','P',' '],
+           [' ',' ',' ',' '],
+           [' ',' ',' ',' '],
+           [' ','P','P','D'],
+           [' ','P','D','Q'],
+           [' ','D','Q','Q']].
 
-display_line([E|Es]):-write(' | '),write(E),display_line(Es).
-display_line([]):-write(' | '),nl.
+/*Funções responsáveis por gerar o tabuleiro vazio de forma dinámica-------------------------------------------------------------------------------------------------------------------*/
 
+generate_line([],0).
+generate_line([E|Es],N):-
+    N > 0,
+    N1 is N - 1,
+    E=' ',
+    generate_line(Es, N1).
+
+generate_board([],0,_).
+generate_board([L|Ls],N,M):-
+  generate_line(L,M),
+  N1 is N - 1,
+  generate_board(Ls, N1, M).
+
+
+/*Funções responsáveis por fazer display do tabuleiro----------------------------------------------------------------------------------------------------------------------------------*/
+display_board([L|Ls],Rows,Columns,I):-
+  TempRows is round(Rows/2),
+  I=TempRows,
+  I1 is I+1,
+  write('  '),
+  display_boundary(Columns),
+  nl,
+  display_line_elems(L,I),
+  nl,
+  display_board(Ls,Rows,Columns,I1).
+
+display_board([L|Ls],Rows,Columns,I):-
+  NumberCols is Columns*5,
+  I1 is I+1,
+  display_horizontal(NumberCols),
+  display_line_elems(L,I),
+  nl,
+  display_board(Ls,Rows,Columns,I1).
+display_board([],Rows,Columns,I):-
+  NumberCols is Columns*5,
+  display_horizontal(NumberCols).
+
+/*Função responsável por dar display dos elementos das listas*/
+display_line_elems([E|Es],I):-
+  write('   '),
+  write(I),
+  write(''),
+  display_elems([E|Es]).
+
+display_elems([E|Es]):-
+  write(' | '),
+  write(E),
+  display_elems(Es).
+display_elems([]):-write(' | ').
+
+/*Display dos indices das colunas*/
+display_cols(Indice,Length):-
+  write('    '),
+  display_cols_indices(Indice,Length).
+
+display_cols_indices(Indice,Length):-
+  Indice<Length,
+  Indice1 is Indice+1,
+  write(' | '),
+  write(Indice),
+  display_cols_indices(Indice1,Length).
+display_cols_indices(Length,Length):-write(' | '),nl.
+
+/*Display das linhas separadoras horizontais*/
 display_horizontal(N):-
+  write('  '),
+  display_horizontal_line(N).
+
+display_horizontal_line(N):-
   N>=1,
   N1 is N-1,
-  write(' '),
-  write('_'),
-  write(' '),
-  display_horizontal(N1).
-display_horizontal(0).
+  write(''),
+  write('-'),
+  write(''),
+  display_horizontal_line(N1).
+display_horizontal_line(0):-nl.
+
+/*Display da fronteira entre ambos os quadrantes*/
+display_boundary(N):-
+  N>=1,
+  N1 is N-1,
+  write(''),
+  write('_____'),
+  write(''),
+  display_boundary(N1).
+display_boundary(0):-nl.
+
+
+/*Funções responsáveis por identificar as peças de jogo e calcular os pontos totais numa só linha--------------------------------------------------------------------------------------*/
 
 piece_value('Q', Value):-Value is 3.
 piece_value('D',Value):-Value is 2.
