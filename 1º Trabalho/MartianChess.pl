03d7cc89276ca088872939949b0817e3e38a0f1b
jpedrotm <j.pedroteixeira.monteiro@gmail.com>
2016-10-28 20:26:05 +0100

    calculate points almost completed

--- 1º Trabalho/MartianChess.pl
+++ 1º Trabalho/MartianChess.pl
@@ -1,22 +1,35 @@
 
 
 main:-
-  Rows = 8, Columns = 4,
-  generate_board(Matrix,Rows,Columns),
-  display_cols(0,Columns),
-  display_board(Matrix,Rows,Columns,0).
+  %Rows = 8, Columns = 4,
+  %generate_board(Matrix,Rows,Columns),
+  initialize_board(Matrix,Columns,Rows),
+  %display_cols(0,Columns),
+  %display_board(Matrix,Rows,Columns,0),
+  calc_players_points(Matrix,Rows,0,0,0).
+
+  /*Retorno de simbolo de cada tipo de peça--------------------------------------------------------------------------------------------------------------------------------------------*/
+
+  piece_simbol('Queen',Simbol):-
+    Simbol is 'A'.
+  piece_simbol('Drone',Simbol):-
+    Simbol is 'a'.
+  piece_simbol('Pawn',Simbol):-
+    Simbol is '.'.
 
   /*Função para inicializar um tabuleiro de forma estática-----------------------------------------------------------------------------------------------------------------------------*/
 
-  initialize_board(Board):-
-    Board=[['Q','Q','D',' '],
-           ['Q','D','P',' '],
-           ['D','P','P',' '],
+  initialize_board(Board,Columns,Rows):-
+    Columns is 4,
+    Rows is 8,
+    Board=[['A','A','a',' '],
+           ['A','a','+',' '],
+           ['a','+','+',' '],
            [' ',' ',' ',' '],
            [' ',' ',' ',' '],
-           [' ','P','P','D'],
-           [' ','P','D','Q'],
-           [' ','D','Q','Q']].
+           [' ','+','+','a'],
+           [' ','+','a','A'],
+           [' ','a','A','A']].
 
 /*Funções responsáveis por gerar o tabuleiro vazio de forma dinámica-------------------------------------------------------------------------------------------------------------------*/
 
@@ -32,6 +45,12 @@ generate_board([L|Ls],N,M):-
   generate_line(L,M),
   N1 is N - 1,
   generate_board(Ls, N1, M).
+
+get_dimensions(Columns,Rows):-
+  write('Indique o numero de colunas: \n'),
+  read(Columns),
+  write('Indique o numero de linhas: \n'),
+  read(Rows).
 
 
 /*Funções responsáveis por fazer display do tabuleiro----------------------------------------------------------------------------------------------------------------------------------*/
@@ -110,10 +129,29 @@ display_boundary(0):-nl.
 
 /*Funções responsáveis por identificar as peças de jogo e calcular os pontos totais numa só linha--------------------------------------------------------------------------------------*/
 
-piece_value('Q', Value):-Value is 3.
-piece_value('D',Value):-Value is 2.
-piece_value('P',Value):-Value is 1.
+piece_value('A', Value):-Value is 3.
+piece_value('a',Value):-Value is 2.
+piece_value('+',Value):-Value is 1.
 piece_value(Rest,Value):-Value is 0.
 
-calc_value_line(Value,[E|Es]):-piece_value(E,Val),Value1 is Val+Value,calc_value_line(Value1,Es).
-calc_value_line(Value,[]):-write(Value).
+/*calc_players_points([L|Ls],NumLines,TotalValue1,TotalValue2,CurrLine):-
+  CurrLine>round(NumLines/2),
+  CurrLine1 is CurrLine+1,
+  calc_value_line(L,Player1),
+  TotalValue1 is TotalValue1+Player1,
+  calc_players_points(Ls,NumLines,TotalValue1,TotalValue2,CurrLine1).
+
+calc_players_points([L|Ls],NumLines,TotalValue1,TotalValue2,CurrLine):-
+  CurrLine1 is CurrLine+1,
+  calc_value_line(L,Player2),
+  TotalValue2 is TotalValue2+Player2,
+  calc_players_points(Ls,NumLines,TotalValue1,TotalValue2,CurrLine1).
+
+calc_players_points([],_,_,_,_).
+*/
+
+sum_line([], FinalValue, FinalValue).
+sum_line([E|Es], CurrValue, FinalValue) :- piece_value(E,Val),CurrValue1 is CurrValue + Val, sum_line(Es, CurrValue1, FinalValue).
+
+calc_value_line([], 0).
+calc_value_line(L, Value) :- sum_line(L,0,Value).
