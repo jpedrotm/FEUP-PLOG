ab6cd412922c9a5afe7d5281bef9d26af35cf8bc
jpedrotm <j.pedroteixeira.monteiro@gmail.com>
2016-10-31 18:25:37 +0000

    calculate points function completed, move function started

--- 1º Trabalho/MartianChess.pl
+++ 1º Trabalho/MartianChess.pl
@@ -4,9 +4,14 @@ main:-
   %Rows = 8, Columns = 4,
   %generate_board(Matrix,Rows,Columns),
   initialize_board(Matrix,Columns,Rows),
-  %display_cols(0,Columns),
-  %display_board(Matrix,Rows,Columns,0),
-  calc_players_points(Matrix,Rows,0,0,0).
+  display_cols(0,Columns),
+  display_board(Matrix,Rows,Columns,0),
+  calc_players_points(Matrix,Rows,PlayerPoints1,PlayerPoints2),
+  write('Player 1: '),
+  write(PlayerPoints1),
+  nl,
+  write('Player 2: '),
+  write(PlayerPoints2).
 
   /*Retorno de simbolo de cada tipo de peça--------------------------------------------------------------------------------------------------------------------------------------------*/
 
@@ -134,24 +139,59 @@ piece_value('a',Value):-Value is 2.
 piece_value('+',Value):-Value is 1.
 piece_value(Rest,Value):-Value is 0.
 
-/*calc_players_points([L|Ls],NumLines,TotalValue1,TotalValue2,CurrLine):-
+
+calc_players_points([L|Ls],NumLines,PlayerPoints1,PlayerPoints2):-
+  calc_points([L|Ls],NumLines,0,PlayerPoints1,0,PlayerPoints2,0).
+
+calc_points([L|Ls],NumLines,CurrPlayerPoints1,PlayerPoints1,CurrPlayerPoints2,PlayerPoints2,CurrLine):-
+  CurrLine<NumLines,
   CurrLine>round(NumLines/2),
   CurrLine1 is CurrLine+1,
-  calc_value_line(L,Player1),
-  TotalValue1 is TotalValue1+Player1,
-  calc_players_points(Ls,NumLines,TotalValue1,TotalValue2,CurrLine1).
+  calc_value_line(L,LinePoints),
+  CurrPlayerPoints11 is CurrPlayerPoints1+LinePoints,
+  calc_points(Ls,NumLines,CurrPlayerPoints11,PlayerPoints1,CurrPlayerPoints2,PlayerPoints2,CurrLine1).
 
-calc_players_points([L|Ls],NumLines,TotalValue1,TotalValue2,CurrLine):-
+calc_points([L|Ls],NumLines,CurrPlayerPoints1,PlayerPoints1,CurrPlayerPoints2,PlayerPoints2,CurrLine):-
+  CurrLine<NumLines,
   CurrLine1 is CurrLine+1,
-  calc_value_line(L,Player2),
-  TotalValue2 is TotalValue2+Player2,
-  calc_players_points(Ls,NumLines,TotalValue1,TotalValue2,CurrLine1).
+  calc_value_line(L,LinePoints),
+  CurrPlayerPoints21 is CurrPlayerPoints2+LinePoints,
+  calc_points(Ls,NumLines,CurrPlayerPoints1,PlayerPoints1,CurrPlayerPoints21,PlayerPoints2,CurrLine1).
 
-calc_players_points([],_,_,_,_).
-*/
+calc_points([],NumLines,FinalPoints1,FinalPoints1,FinalPoints2,FinalPoints2,NumLines).
 
 sum_line([], FinalValue, FinalValue).
 sum_line([E|Es], CurrValue, FinalValue) :- piece_value(E,Val),CurrValue1 is CurrValue + Val, sum_line(Es, CurrValue1, FinalValue).
 
 calc_value_line([], 0).
 calc_value_line(L, Value) :- sum_line(L,0,Value).
+
+/*Funções responsáveis por mover peças de jogo e a sua verificação -------------------------------------------------------------------------------------------------------------------*/
+move([L|Ls],Xi,Yi,Xf,Yf):-
+  verify_cell([L|Ls],0,0,Xi,Yi,Cell),
+  write(Cell).
+
+verify_cell([L|Ls],Xi,Yi,Xf,Yf,Cell):-
+  Yi<Yf,
+  Yi1 is Yi+1,
+  verify_cell(Ls,Xi,Yi1,Xf,Yf,Cell).
+
+verify_cell([],_,_,_,_,Cell):-
+  write('Fim de lista de listas, valor de Y demasiado elevado.\n').
+
+verify_cell([L|Ls],Xi,Yi,Xf,Yf,Cell):-
+  Yi=Yf,
+  write('INTO row_cell.'),
+  verify_row_cell(L,0,Xf,Cell).
+
+verify_row_cell([E|Es],Xi,Xf,Cell):-
+  Xi<Xf,
+  Xi1 is Xi+1,
+  verify_row_cell(Es,Xi1,Xf,Cell).
+
+verify_row_cell([E|Es],Xi,Xf,Cell):-
+  Xi=Xf,
+  Cell is E.
+
+verify_row_cell([],_,_,Cell):-
+  write('Fim da lista de elementos, valor de X demasiado grande.\n').
