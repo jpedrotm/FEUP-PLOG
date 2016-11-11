456064ed803e4d9bb1eba35f0f6978ebbbe0c439
jpedrotm <j.pedroteixeira.monteiro@gmail.com>
2016-11-11 15:57:15 +0000

    pawn move completed. Menu started

---
+++ 1º Trabalho/Logic.pl
@@ -0,0 +1,176 @@
+/*Retorno de simbolo de cada tipo de peça--------------------------------------------------------------------------------------------------------------------------------------------*/
+
+  piece_simbol(queen,'A').
+  piece_simbol(drone,'a').
+  piece_simbol(pawn,'+').
+  piece_simbol(empty,' ').
+
+  /*Função para inicializar um tabuleiro de forma estática-----------------------------------------------------------------------------------------------------------------------------*/
+
+  initialize_board(Board,Columns,Rows):-
+    Columns is 4,
+    Rows is 8,
+    Board=[[queen,queen,drone,empty],
+           [queen,drone,pawn,empty],
+           [drone,pawn,pawn,empty],
+           [empty,empty,empty,empty],
+           [empty,empty,empty,empty],
+           [empty,pawn,pawn,drone],
+           [empty,pawn,drone,queen],
+           [empty,drone,queen,queen]].
+
+/*Funções responsáveis por gerar o tabuleiro vazio de forma dinámica-------------------------------------------------------------------------------------------------------------------*/
+generate_line([],0).
+
+generate_line([E|Es],N):-
+ N > 0,
+ N1 is N - 1,
+ E=pawn,
+ generate_line(Es, N1).
+
+ generate_board([],0,_).
+
+generate_board([L|Ls],N,M):-
+  generate_line(L,M),
+  N1 is N - 1,
+  generate_board(Ls, N1, M).
+
+get_dimensions(Columns,Rows):-
+  write('Indique o numero de colunas: \n'),
+  read(Columns),
+  write('Indique o numero de linhas: \n'),
+  read(Rows).
+
+  /*Funções responsáveis por identificar as peças de jogo e calcular os pontos totais numa só linha--------------------------------------------------------------------------------------*/
+
+piece_value(queen, Value):-Value is 3.
+piece_value(drone,Value):-Value is 2.
+piece_value(pawn,Value):-Value is 1.
+piece_value(Rest,Value):-Value is 0.
+
+
+calc_players_points([L|Ls],NumLines,PlayerPoints1,PlayerPoints2):-
+  calc_points([L|Ls],NumLines,0,PlayerPoints1,0,PlayerPoints2,0).
+
+calc_points([L|Ls],NumLines,CurrPlayerPoints1,PlayerPoints1,CurrPlayerPoints2,PlayerPoints2,CurrLine):-
+    CurrLine<NumLines,
+    CurrLine>round(NumLines/2),
+    CurrLine1 is CurrLine+1,
+    calc_value_line(L,LinePoints),
+    CurrPlayerPoints11 is CurrPlayerPoints1+LinePoints,
+    calc_points(Ls,NumLines,CurrPlayerPoints11,PlayerPoints1,CurrPlayerPoints2,PlayerPoints2,CurrLine1).
+
+calc_points([L|Ls],NumLines,CurrPlayerPoints1,PlayerPoints1,CurrPlayerPoints2,PlayerPoints2,CurrLine):-
+    CurrLine<NumLines,
+    CurrLine1 is CurrLine+1,
+    calc_value_line(L,LinePoints),
+    CurrPlayerPoints21 is CurrPlayerPoints2+LinePoints,
+    calc_points(Ls,NumLines,CurrPlayerPoints1,PlayerPoints1,CurrPlayerPoints21,PlayerPoints2,CurrLine1).
+
+calc_points([],NumLines,FinalPoints1,FinalPoints1,FinalPoints2,FinalPoints2,NumLines).
+
+sum_line([], FinalValue, FinalValue).
+sum_line([E|Es], CurrValue, FinalValue) :- piece_value(E,Val),CurrValue1 is CurrValue + Val, sum_line(Es, CurrValue1, FinalValue).
+
+calc_value_line([], 0).
+calc_value_line(L, Value) :- sum_line(L,0,Value).
+
+
+  /*Funções responsáveis por mover peças de jogo e a sua verificação -------------------------------------------------------------------------------------------------------------------*/
+move(Board,Xi,Yi,Xf,Yf,NewBoard):-
+           % Para o caso da a célula para onde quer mexer a peça estar para trás da célula
+    Yf<Yi, % onde se encontra.
+    write('YF<YI\n'),
+    verify_initial_cell(Board,0,0,Xf,Yf,CellFinal),
+    write('Casa final: '), write(CellFinal),nl,
+    verify_initial_cell(Board,0,0,Xi,Yi,CellInitial),
+    write('Case inicial: '),write(CellInitial),nl,
+    place_cell(Board,NewBoard,Xi,Yi,Xf,Yf,CellInitial),
+    display_board(NewBoard,8,4,0).
+
+move(Board,Xi,Yi,Xf,Yf):-
+    Yi=Yf,
+    Xf<Xi,
+    write('XF<XI   YF<YI\n'),
+    verify_initial_cell(Board,0,0,Xf,Yf,CellFinal),
+    write('Casa final: '), write(CellFinal),nl,
+    verify_initial_cell(Board,0,0,Xi,Yi,CellInitial),
+    write('Case inicial: '),write(CellInitial),nl,
+    place_cell(Board,NewBoard,Xi,Yi,Xf,Yf,CellInitial),
+    display_board(NewBoard,8,4,0).
+
+move(Board,Xi,Yi,Xf,Yf):-
+    Yf>=Yi,
+    verify_initial_cell(Board,0,0,Xf,Yf,CellInitial),
+    write('Casa inicial: '), write(CellInitial),nl,
+    verify_initial_cell(Board,0,0,Xi,Yi,CellFinal),
+    write('Case final: '),write(CellFinal),nl,
+    place_cell(Board,NewBoard,Xi,Yi,Xf,Yf,CellFinal),
+    display_board(NewBoard,8,4,0).
+
+place_cell(Board,NewBoard,Xi,Yi,Xf,Yf,pawn):-
+    XDif is Xf-Xi,
+    YDif is Yf-Yi,
+    XMod is XDif*XDif,
+    YMod is YDif*YDif,
+    XMod=1,
+    YMod=1,
+    write('VOU PARA O SWITCH\n'),
+    putOnBoard(Yi,Xi,empty,Board,Board1),
+    putOnBoard(Yf,Xf,pawn,Board1,NewBoard),
+    Changed is 1,
+    write('VALIDOU\n');
+    NewBoard = Board.
+
+place_cell(Board,NewBoard,Xi,Yi,Xf,Yf,drone):-
+  Xi=Xf,
+  YDif is Yf-Yi,
+  YDif>0,
+  YDif<=2.
+
+
+
+
+place_cell(Board,NewBoard,Xi,Yi,Xf,Yf,Cell):-
+    write('CELULA: '),write(Cell),nl,
+    write('Not a valid Cell.\n'),
+    NewBoard = Board.
+
+putOnBoard(0, ElemCol, NewElem, [RowAtTheHead|RemainingRows], [NewRowAtTheHead|RemainingRows]):-
+  	putOnColumn(ElemCol, NewElem, RowAtTheHead, NewRowAtTheHead).
+
+putOnBoard(ElemRow, ElemCol, NewElem, [RowAtTheHead|RemainingRows], [RowAtTheHead|ResultRemainingRows]):-
+  	ElemRow > 0,
+  	ElemRow1 is ElemRow - 1,
+  	putOnBoard(ElemRow1, ElemCol, NewElem, RemainingRows, ResultRemainingRows).
+
+putOnColumn(0, Elem, [_|L], [Elem|L]).
+
+putOnColumn(I, Elem, [H|L], [H|ResL]):-
+  	I > 0,
+  	I1 is I - 1,
+  	putOnColumn(I1, Elem, L, ResL).
+
+verify_initial_cell([L|Ls],Xi,Yi,Xf,Yf,Cell):-
+    Yi<Yf,
+    Yi1 is Yi+1,
+    verify_initial_cell(Ls,Xi,Yi1,Xf,Yf,Cell).
+
+verify_initial_cell([],_,_,_,_,Cell):-
+    write('Fim de lista de listas, valor de Y demasiado elevado.\n').
+
+verify_initial_cell([L|Ls],Xi,Yi,Xf,Yf,Cell):-
+    Yi=Yf,
+    write('\nINTO row_cell.'),
+    verify_row_cell(L,0,Xf,Cell).
+
+verify_row_cell([E|Es],Xi,Xf,Cell):-
+    Xi<Xf,
+    Xi1 is Xi+1,
+    verify_row_cell(Es,Xi1,Xf,Cell).
+
+verify_row_cell([E|Es],Xf,Xf,Cell):-
+    Cell=E.
+
+verify_row_cell([],_,_,Cell):-
+    write('Fim da lista de elementos, valor de X demasiado grande.\n').
--- 1º Trabalho/MartianChess.pl
+++ 1º Trabalho/MartianChess.pl
@@ -1,8 +1,33 @@
-
+:-include('Logic.pl').
+:-include('Interface.pl').
 
 main:-
-  %Rows = 8, Columns = 4,
-  %generate_board(Matrix,Rows,Columns),
+  write('-------------------------------------------------\n'),
+  write('----------------- MARTIAN CHESS -----------------\n'),
+  write('-------------------------------------------------\n'),
+  write('------------- 1 - Player vs Player. -------------\n'),
+  write('--------------- 2 - Player vs cpu. --------------\n'),
+  write('--------------- 3 - cpu vs cpu. -----------------\n'),
+  write('------------------ 4 - Exit.  -------------------\n'),
+  write('-------------------------------------------------\n'),
+  !,
+  nl,
+  write('Insert an option: '),
+  read(Opt),
+	(
+		Opt = 1 -> player_vs_player;
+		Opt = 2 -> player_vs_player;
+		Opt = 3 -> player_vs_player;
+    Opt = 4;
+    nl,
+		write(Opt), nl,
+		write('Not a valid input. Choose again.\n'),
+    main
+	).
+
+
+player_vs_player:-
+  nl,
   initialize_board(Matrix,Columns,Rows),
   display_cols(0,Columns),
   display_board(Matrix,Rows,Columns,0),
@@ -11,187 +36,6 @@ main:-
   write(PlayerPoints1),
   nl,
   write('Player 2: '),
-  write(PlayerPoints2).
-
-  /*Retorno de simbolo de cada tipo de peça--------------------------------------------------------------------------------------------------------------------------------------------*/
-
-  piece_simbol('Queen',Simbol):-
-    Simbol is 'A'.
-  piece_simbol('Drone',Simbol):-
-    Simbol is 'a'.
-  piece_simbol('Pawn',Simbol):-
-    Simbol is '.'.
-
-  /*Função para inicializar um tabuleiro de forma estática-----------------------------------------------------------------------------------------------------------------------------*/
-
-  initialize_board(Board,Columns,Rows):-
-    Columns is 4,
-    Rows is 8,
-    Board=[['A','A','a',' '],
-           ['A','a','+',' '],
-           ['a','+','+',' '],
-           [' ',' ',' ',' '],
-           [' ',' ',' ',' '],
-           [' ','+','+','a'],
-           [' ','+','a','A'],
-           [' ','a','A','A']].
-
-/*Funções responsáveis por gerar o tabuleiro vazio de forma dinámica-------------------------------------------------------------------------------------------------------------------*/
-
-generate_line([],0).
-generate_line([E|Es],N):-
-    N > 0,
-    N1 is N - 1,
-    E=' ',
-    generate_line(Es, N1).
-
-generate_board([],0,_).
-generate_board([L|Ls],N,M):-
-  generate_line(L,M),
-  N1 is N - 1,
-  generate_board(Ls, N1, M).
-
-get_dimensions(Columns,Rows):-
-  write('Indique o numero de colunas: \n'),
-  read(Columns),
-  write('Indique o numero de linhas: \n'),
-  read(Rows).
-
-
-/*Funções responsáveis por fazer display do tabuleiro----------------------------------------------------------------------------------------------------------------------------------*/
-display_board([L|Ls],Rows,Columns,I):-
-  TempRows is round(Rows/2),
-  I=TempRows,
-  I1 is I+1,
-  write('  '),
-  display_boundary(Columns),
+  write(PlayerPoints2),
   nl,
-  display_line_elems(L,I),
-  nl,
-  display_board(Ls,Rows,Columns,I1).
-
-display_board([L|Ls],Rows,Columns,I):-
-  NumberCols is Columns*5,
-  I1 is I+1,
-  display_horizontal(NumberCols),
-  display_line_elems(L,I),
-  nl,
-  display_board(Ls,Rows,Columns,I1).
-display_board([],Rows,Columns,I):-
-  NumberCols is Columns*5,
-  display_horizontal(NumberCols).
-
-/*Função responsável por dar display dos elementos das listas*/
-display_line_elems([E|Es],I):-
-  write('   '),
-  write(I),
-  write(''),
-  display_elems([E|Es]).
-
-display_elems([E|Es]):-
-  write(' | '),
-  write(E),
-  display_elems(Es).
-display_elems([]):-write(' | ').
-
-/*Display dos indices das colunas*/
-display_cols(Indice,Length):-
-  write('    '),
-  display_cols_indices(Indice,Length).
-
-display_cols_indices(Indice,Length):-
-  Indice<Length,
-  Indice1 is Indice+1,
-  write(' | '),
-  write(Indice),
-  display_cols_indices(Indice1,Length).
-display_cols_indices(Length,Length):-write(' | '),nl.
-
-/*Display das linhas separadoras horizontais*/
-display_horizontal(N):-
-  write('  '),
-  display_horizontal_line(N).
-
-display_horizontal_line(N):-
-  N>=1,
-  N1 is N-1,
-  write(''),
-  write('-'),
-  write(''),
-  display_horizontal_line(N1).
-display_horizontal_line(0):-nl.
-
-/*Display da fronteira entre ambos os quadrantes*/
-display_boundary(N):-
-  N>=1,
-  N1 is N-1,
-  write(''),
-  write('_____'),
-  write(''),
-  display_boundary(N1).
-display_boundary(0):-nl.
-
-
-/*Funções responsáveis por identificar as peças de jogo e calcular os pontos totais numa só linha--------------------------------------------------------------------------------------*/
-
-piece_value('A', Value):-Value is 3.
-piece_value('a',Value):-Value is 2.
-piece_value('+',Value):-Value is 1.
-piece_value(Rest,Value):-Value is 0.
-
-
-calc_players_points([L|Ls],NumLines,PlayerPoints1,PlayerPoints2):-
-  calc_points([L|Ls],NumLines,0,PlayerPoints1,0,PlayerPoints2,0).
-
-calc_points([L|Ls],NumLines,CurrPlayerPoints1,PlayerPoints1,CurrPlayerPoints2,PlayerPoints2,CurrLine):-
-  CurrLine<NumLines,
-  CurrLine>round(NumLines/2),
-  CurrLine1 is CurrLine+1,
-  calc_value_line(L,LinePoints),
-  CurrPlayerPoints11 is CurrPlayerPoints1+LinePoints,
-  calc_points(Ls,NumLines,CurrPlayerPoints11,PlayerPoints1,CurrPlayerPoints2,PlayerPoints2,CurrLine1).
-
-calc_points([L|Ls],NumLines,CurrPlayerPoints1,PlayerPoints1,CurrPlayerPoints2,PlayerPoints2,CurrLine):-
-  CurrLine<NumLines,
-  CurrLine1 is CurrLine+1,
-  calc_value_line(L,LinePoints),
-  CurrPlayerPoints21 is CurrPlayerPoints2+LinePoints,
-  calc_points(Ls,NumLines,CurrPlayerPoints1,PlayerPoints1,CurrPlayerPoints21,PlayerPoints2,CurrLine1).
-
-calc_points([],NumLines,FinalPoints1,FinalPoints1,FinalPoints2,FinalPoints2,NumLines).
-
-sum_line([], FinalValue, FinalValue).
-sum_line([E|Es], CurrValue, FinalValue) :- piece_value(E,Val),CurrValue1 is CurrValue + Val, sum_line(Es, CurrValue1, FinalValue).
-
-calc_value_line([], 0).
-calc_value_line(L, Value) :- sum_line(L,0,Value).
-
-/*Funções responsáveis por mover peças de jogo e a sua verificação -------------------------------------------------------------------------------------------------------------------*/
-move([L|Ls],Xi,Yi,Xf,Yf):-
-  verify_cell([L|Ls],0,0,Xi,Yi,Cell),
-  write(Cell).
-
-verify_cell([L|Ls],Xi,Yi,Xf,Yf,Cell):-
-  Yi<Yf,
-  Yi1 is Yi+1,
-  verify_cell(Ls,Xi,Yi1,Xf,Yf,Cell).
-
-verify_cell([],_,_,_,_,Cell):-
-  write('Fim de lista de listas, valor de Y demasiado elevado.\n').
-
-verify_cell([L|Ls],Xi,Yi,Xf,Yf,Cell):-
-  Yi=Yf,
-  write('INTO row_cell.'),
-  verify_row_cell(L,0,Xf,Cell).
-
-verify_row_cell([E|Es],Xi,Xf,Cell):-
-  Xi<Xf,
-  Xi1 is Xi+1,
-  verify_row_cell(Es,Xi1,Xf,Cell).
-
-verify_row_cell([E|Es],Xi,Xf,Cell):-
-  Xi=Xf,
-  Cell is E.
-
-verify_row_cell([],_,_,Cell):-
-  write('Fim da lista de elementos, valor de X demasiado grande.\n').
+  move(Matrix,2,2,2,3).
