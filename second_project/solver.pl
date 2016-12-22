b5973b838fe842927eac011a86383db9fa1a0fd0
jpedrotm <j.pedroteixeira.monteiro@gmail.com>
2016-12-22 17:27:52 +0000

    convert from matrix to list and get points list

--- second_project/solver.pl
+++ second_project/solver.pl
@@ -1,18 +1,18 @@
 :-use_module(library(clpfd)).
 :-use_module(library(lists)).
 
-% [-1,-1,-1,-1,10,-1,-1,-1,-1,-1,-1,-1],
-% [2,-1,-1,-1,-1,-1,5,-1,-1,-1,-1,-1],
-% [-1,-1,6,-1,-1,-1,-1,-1,-1,-1,7,-1],
-% [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,12],
-% [-1,-1,-1,9,-1,-1,-1,-1,-1,-1,1,-1],
-% [-1,-1,-1,-1,-1,-1,-1,-1,7,-1,-1,-1],
-% [-1,4,-1,-1,-1,-1,-1,8,-1,-1,-1,-1],
-% [9,-1,-1,-1,1,-1,-1,-1,-1,-1,4,-1],
-% [-1,5,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
-% [-1,-1,-1,-1,-1,-1,6,-1,-1,1,-1,-1],
-% [-1,-1,8,-1,-1,-1,-1,-1,-1,-1,-1,6],
-% [-1,-1,-1,-1,-1,-1,-1,-1,13,-1,-1,-1]
+board:- [[-1,-1,-1,-1,10,-1,-1,-1,-1,-1,-1,-1],
+ [2,-1,-1,-1,-1,-1,5,-1,-1,-1,-1,-1],
+ [-1,-1,6,-1,-1,-1,-1,-1,-1,-1,7,-1],
+ [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,12],
+ [-1,-1,-1,9,-1,-1,-1,-1,-1,-1,1,-1],
+ [-1,-1,-1,-1,-1,-1,-1,-1,7,-1,-1,-1],
+ [-1,4,-1,-1,-1,-1,-1,8,-1,-1,-1,-1],
+ [9,-1,-1,-1,1,-1,-1,-1,-1,-1,4,-1],
+ [-1,5,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+ [-1,-1,-1,-1,-1,-1,6,-1,-1,1,-1,-1],
+ [-1,-1,8,-1,-1,-1,-1,-1,-1,-1,-1,6],
+ [-1,-1,-1,-1,-1,-1,-1,-1,13,-1,-1,-1]].
 
  get_board([5-1-10,
           1-2-2,
@@ -35,6 +35,28 @@
           12-11-8,
           9-12-13]).
 
+get_board_points(Board,Points):-
+  get_board_points(Board,1,[],Points).
+
+get_board_points([M|Ms],LineNumber,CurrPoints,FinalPoints):-
+  get_line_points(M,LineNumber,1,LinePoints),
+  append(CurrPoints,LinePoints,CurrPoints1),
+  LineNumber1 is LineNumber+1,
+  get_board_points(Ms,LineNumber1,CurrPoints1,FinalPoints).
+
+get_board_points([],_,FinalPoints,FinalPoints).
+
+get_line_points([],_,_,[]).
+
+get_line_points([-1|Tail], LineNr, ColNr, Points):-
+  NextColNr is ColNr + 1,
+  get_line_points(Tail, LineNr, NextColNr, Points).
+
+get_line_points([Head|Tail], LineNr, ColNr, [ColNr-LineNr-Head|Rest]):-
+  Head \= -1,
+  NextColNr is ColNr + 1,
+  get_line_points(Tail, LineNr, NextColNr, Rest).
+
 force_solution_size([S|Ss],N,NoIndexes):-
   length(S,N),
   domain(S,0,NoIndexes),
@@ -49,17 +71,12 @@ get_line([_|Rs], LineNr, N, Line):-
   N1 is N + 1,
   get_line(Rs, LineNr, N1, Line).
 
-flatten([],[]).
-flatten([LH|LT], Flattened) :-
-	is_list(LH),
-	flatten(LH, Aux),
-	append(Aux, LT2, Flattened),
-	flatten(LT, LT2).
+matrix_to_list([M|Ms],CurrList,FinalList):-
+  is_list(M),
+  append(M,CurrList,CurrList1),
+  matrix_to_list(Ms,CurrList1,FinalList).
 
-flatten([LH | LT], [LH | FlattenedT]) :-
-	\+is_list(LH),
-	flatten(LT, FlattenedT).
-
+matrix_to_list([],FinalList,FinalList).
 
 force_adjacency(Line, InitialNr, Rep):-
   force_adjacency(Line, InitialNr, Rep, 1).
@@ -104,7 +121,7 @@ make_no_appearances(Solution, LineNr, ColNr, Representation, NoAppearances):-
   force_adjacency(Col, LineNr, Representation),
   TotalAppearances is NoAppearances+1,
   LineAppearances #= TotalAppearances+1-ColAppearances, %+1 because the line and column have the element repeated
-  flatten(Solution, Flattened),
+  matrix_to_list(Solution,[],Flattened),
   count(Representation, Flattened, #=, TotalAppearances). % make the nr of appearances on other lines and columns 0
 
 go_through_board([], _, _, _).
@@ -114,14 +131,12 @@ go_through_board([Head|Rest], Result, Size, Representation):-
   Representation1 is Representation +  1,
   go_through_board(Rest, Result, Size, Representation1).
 
-
-
 % receives a list of : X-Y-Nr, where nr is the number of blocks we want that block to expand to
 solve_prob(Points, N, Result):-
   length(Result, N),
   length(Points, NoIndexes),
   force_solution_size(Result, N, NoIndexes),
   go_through_board(Points, Result, N, 1),
-  flatten(Result, FlatResult),
+  matrix_to_list(Result,[],FlatResult),
   labeling([], FlatResult),
   fd_statistics.
